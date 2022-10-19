db8b7f4b47a02586a0059fdedf5dec3508dd2eab
tobby-t <tobilobalawal25@gmail.com>
2022-10-19 06:25:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: Returns 0 if no error, non-zero value if error
+ */
+int main(void)
+{
+	printf("_putchar /r/n")
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+a program that prints _putchar, followed by a new line.
+
