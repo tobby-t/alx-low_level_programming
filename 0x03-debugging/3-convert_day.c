03d70614cf044c180f88c5f4cec8ac2027f155fa
tobby-t <tobilobalawal25@gmail.com>
2022-10-20 00:36:32 -0700

    pushing

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
 /**
-
   * convert_day - converts day of month to day of year, without accounting
-
   * for leap year
-
   * @month: month in number format
-
   * @day: day of month
-
   * Return: day of year
  */
 
